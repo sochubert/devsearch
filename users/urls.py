412bfb5d05adcd3cf6cec9bc711ae3f257d3cfa5
sochubert <sochubert@gmail.com>
2023-02-14 14:07:18 +0900

    User Registration

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     path('login/', views.loginPage, name='login'),
-    path('logout/', views.logoutUser, name='logout')
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerUser, name='register')
 ]
