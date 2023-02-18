8d1acd388919e2b99469919d41ea8347923b573b
sochubert <sochubert@gmail.com>
2023-02-18 23:28:47 +0900

    Send email when register

--- users/signals.py
+++ users/signals.py
@@ -3,6 +3,9 @@ from .models import Profile
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 # @receiver(post_save, sender=Profile)
 
 
@@ -15,6 +18,17 @@ def createProfile(sender, instance, created, **kwargs):
             username=user.username,
             email=user.email,
             name=user.first_name,
+        )
+
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you are here!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
         )
 
 
