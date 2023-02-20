bee61c8de3c76d81f80a6a9a19b0a02361e19bdd
sochubert <sochubert@gmail.com>
2023-02-20 16:47:40 +0900

    User deleted & Deleted user's project bug fixes

--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,7 @@ from users.models import Profile
 
 class Project(models.Model):
     owner = models.ForeignKey(
-        Profile, null=True, blank=True, on_delete=models.SET_NULL)
+        Profile, null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(
